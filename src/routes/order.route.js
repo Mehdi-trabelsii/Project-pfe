275b8a8dc49b9339138f70b9d06945ff4c9ab51b
Mehdi-trabelsii <60862725+Mehdi-trabelsii@users.noreply.github.com>
2020-09-07 13:44:13 +0100

    order fixes 2.0

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import * as controller from '../controllers/order.controller';
-import { authorize, LOGGED_USER } from '../middlewares/auth';
+import { authorize, LOGGED_USER,ADMIN } from '../middlewares/auth';
 import validation from '../middlewares/validation';
 const router = express.Router();
 
 router.route('/create').post(authorize(LOGGED_USER), controller.create);
 router.route('/list/:id').get(controller.get)
 router.route('/list').get(controller.list)
-
+router.route('/update/:id').patch(authorize(LOGGED_USER),controller.update);
 
 export default router;
