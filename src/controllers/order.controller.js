c8b4de7ac57a7e6b2c97de62fae4ffc0917e53a3
Mehdi-trabelsii <60862725+Mehdi-trabelsii@users.noreply.github.com>
2020-09-07 13:41:08 +0100

    list order fixes

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,13 +1,21 @@
 import Order from '../models/order.model';
 import Cart from '../models/cart.model';
 import ApiResponse from '../utils/APIResponse';
+import httpStatus from 'http-status';
+import { transform } from 'lodash';
+
 
 
 export function get(req, res) {
     return new ApiResponse(res).success(() => {
-        const order = Order.findById(req.params.id).populate('cart.products');
-        console.log(order)
-        return order;
+        const orders = Order.findById(req.params.id).populate('user').populate('cart.products');
+        const transformedOrder = orders.map(orders => {
+            let order = orders.transform();
+    
+            return order;
+          });;
+        console.log(transformedOrder)
+        return transformedOrder;
     });
 }
 
@@ -15,7 +23,7 @@ export function list(req, res, next) {
     return new ApiResponse(res).success(
         async () => {
 
-            const orders = await Order.find(req.query).populate('cart.products.product');
+            const orders = await Order.find(req.query).populate('user').populate('cart.products');
             console.log(orders);
             const transformedOrders = orders.map(order => order.transform());
             return transformedOrders;
@@ -29,7 +37,12 @@ export function create(req, res, next) {
         async () => {
             const { user } = req.locals;
             let cart = await Cart.findOne({ user: user._id })
-            var order = new Order({ products: cart.products, date: Date.now(), adresse: req.body.adresse })
+            var order = new Order({
+                products: cart.products,
+                date: Date.now(),
+                adresse: req.body.adresse,
+                user: user._id
+            });
             await cart.update({ products: [] })
             console.log(cart)
             await order.save()
